70302264f98c8cb3542421a808a18f1dc8429ac5
Jimmy Leroy <jleroy@sqli.com>
2017-08-31 17:22:14 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import _ from 'lodash';
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/searchBar"
+import VideoList from "./components/videoList"
+import VideoDetail from "./components/videoDetail"
+
+const API_KEY  = "AIzaSyDYJ5M6YC6o_rx9Qa6RxZfmgKhLHchEhNg";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state =  {
+      videos : [],
+      selectedVideo : null
+    }
+
+    this.videoSearch("prank")
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos : videos,
+        selectedVideo : videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce(
+      (term) => { this.videoSearch(term) }, 300
+    )
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange = { videoSearch } />
+        <VideoDetail video = { this.state.selectedVideo } />
+        <VideoList 
+          onVideoSelect = { 
+            (selectedVideo) => this.setState({ selectedVideo })
+          }
+          videos = { this.state.videos } />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector(".container")
+)
